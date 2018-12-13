589558f407e455cd2c08240501decd475a1bc68a
NGE <nge@yangyuxiaodemacbook-pro.local>
2018-12-13 10:55:18 +0800

    Initial Commit

---
+++ Goroskop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Goroskop
+//
+//  Created by NGE on 2018/12/13.
+//  Copyright © 2018年 NGE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoroskopTests/GoroskopTests.m
@@ -0,0 +1,37 @@
+//
+//  GoroskopTests.m
+//  GoroskopTests
+//
+//  Created by NGE on 2018/12/13.
+//  Copyright © 2018年 NGE. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GoroskopTests : XCTestCase
+
+@end
+
+@implementation GoroskopTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
